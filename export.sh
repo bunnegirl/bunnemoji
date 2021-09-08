ea67a8d99adafc42c579a74204e8506302b25a9a
bunnegirl <hi@bunnegirl.solutions>
2021-09-08 17:50:35 +0930

    added square export option as some platforms (misskey) expect that

--- export.sh
+++ export.sh
@@ -2,9 +2,11 @@
 
 LAYERS=$(xmlstarlet sel -t -v "//svg:g[@inkscape:groupmode = 'layer']/@id" bunne.svg)
 
-mkdir -p ./export
+mkdir -p ./export/trimmed
+mkdir -p ./export/squared
 
 for layer in $LAYERS
 do
-    inkscape --export-id $layer --export-filename=export/$layer.png --export-overwrite --export-id-only bunne.svg
+    inkscape --export-id $layer --export-filename=export/trimmed/$layer.png --export-overwrite --export-id-only bunne.svg
+    inkscape --export-id $layer --export-filename=export/squared/$layer.png --export-overwrite --export-id-only --export-area=-35:-35:291:291 bunne.svg
 done
